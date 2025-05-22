3552eab188986b582b26dbb59c7a255319b2a57f
s26621 <s26621@pjwstk.edu.pl>
2025-05-22 23:12:09 +0200

    WebConfig api key placeholder

--- src/main/java/edu/pja/sii_lat/config/WebClientConfig.java
+++ src/main/java/edu/pja/sii_lat/config/WebClientConfig.java
@@ -9,7 +9,7 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         // enter Your key here
-        String apiKey = "590f0620afc981a78315873c";
+        String apiKey = "YOUR_KEY";
         String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/";
         return WebClient.builder()
                 .baseUrl(url)
